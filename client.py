46943ef13de66d9d9bb8cf92e308b20e1af7bff9
Dogggoggo <73349@parade.vic.edu.au>
2025-05-13 15:15:14 +1000

    squidward 5

--- client.py
+++ client.py
@@ -1,4 +1,5 @@
 """
+
 to do:
 make gui DONE
 when exit signal is called; cleanly closes connections
@@ -13,6 +14,11 @@ connect to server
 spam limit
 client based profanity filtering
 add image support (pls dear god no)
+
+
+
+
+
 """
 
 from PySide6.QtCore import QSize, Qt, QThread
@@ -22,23 +28,28 @@ from PySide6.QtCore import *
 import _thread, sys, time, threading, json
 import socket as s
 
-# networking part
+
+
+
+
+
 # start of gui creation, oh boy
 
-class UISignals():
-    log = Signal(str)
-    userlst = Signal(str)
+
 
 class MainWindow(QMainWindow):
+    
     def __init__(self): # when initialised
         super().__init__() # it was in the tutorial -_- canon event fr
+        self.receivedMessage = {}
         self.setWindowTitle("Rizzcord for Nerds") # Sets window title
         self.setMinimumSize(1200,600)
+
         my_icon = QIcon()
         my_icon.addFile('images\\image.png')
         self.setWindowIcon(my_icon) # sets window icon
         self.initialiseWidgets()
-
+        self.messagesdisplay.append("Welcome to Rizzcord. \nOnce you have joined a server you may type /help to view the commands.")
 
     def initialiseWidgets(self): # new function just to seperate the creation and initialization of widgets
         
@@ -54,21 +65,26 @@ class MainWindow(QMainWindow):
         userListTitle = QLabel("Users:") # creates the title
         userListTitle.setStyleSheet("font-weight: bold; font-size: 16px;")
         userListMain.addWidget(userListTitle)
-        self.userList = QTextEdit() #creates log text box
-        self.userList.setReadOnly(True)
-        #self.userList = QLabel("User1\nUser2\nStupid Idiot mf udawgduahiodwjhaodjhwaoidjwaoidwa") # creates a larger text box 
+        self.userList = QTableWidget() #creates log text box
+        self.userList.setColumnCount(2)
+        self.userList.setHorizontalHeaderLabels(["User", "Actions"])
+        self.userList.horizontalHeader().setStretchLastSection(True)
+        
         # User list should update when users change
+
         userListMain.addWidget(self.userList)
         infoColumnnew.addLayout(userListMain)
 
+
         # Server/User info
+
         # IP
         serverIPMain = QHBoxLayout()
         ipTitle = QLabel("Server IP: ")
         serverIPMain.addWidget(ipTitle)
         self.ipEntry = QLineEdit()
         serverIPMain.addWidget(self.ipEntry)
-        self.ipEntry.setText(s.gethostbyname(s.gethostname()))
+        self.ipEntry.setText("127.0.0.1")
         infoColumnnew.addLayout(serverIPMain)
 
         # Port (pretty much copy and paste of IP)
@@ -90,21 +106,29 @@ class MainWindow(QMainWindow):
         self.usernameEntry = QLineEdit()
         usernameMain.addWidget(self.usernameEntry)
         infoColumnnew.addLayout(usernameMain)
-        
-        # Colour
+
+        # Colour, unfortunately can't be included
+        '''
         coloursMain = QHBoxLayout()
         coloursTitle = QLabel("Colours: ")
         coloursMain.addWidget(coloursTitle)
         self.coloursOptions = QComboBox()
         self.coloursOptions.addItems(["Red", "Blue", "1", "2", "3"])
         coloursMain.addWidget(self.coloursOptions)
-        infoColumnnew.addLayout(coloursMain)
+        infoColumnnew.addLayout(coloursMain)'''
 
         # Connect
         self.connected = False
         self.connectButton = QPushButton("Connect")
         self.connectButton.clicked.connect(self.connectToServer)
         infoColumnnew.addWidget(self.connectButton)
+
+        # Sample button for testing purposes
+        '''
+        self.testButton = QPushButton("Test")
+        self.testButton.clicked.connect(self.setText)
+        '''
+        
 
         # Messages area
         self.messagesdisplay = QTextEdit() #creates log text box
@@ -116,23 +140,29 @@ class MainWindow(QMainWindow):
         self.messageEntry = QLineEdit()
         self.messageEntry.setPlaceholderText('Enter Message')
         self.messageEntry.returnPressed.connect(self.sendMessage)
-        self.messageEntry.setEnabled(False)
+        self.messageEntry.setEnabled(True)
         messageEntryMain.addWidget(self.messageEntry)
         self.sendButton = QPushButton("Send")
         self.sendButton.clicked.connect(self.sendMessage)
-        self.sendButton.setEnabled(False)
+        self.sendButton.setEnabled(True)
         messageEntryMain.addWidget(self.sendButton)
         messageColumn.addLayout(messageEntryMain, 1, 0)
 
+
+
         # places the two main columns
+
         infoColumnnew.addLayout(infoColumn)
         messageColumnnew.addLayout(messageColumn)
         
+
+        
         mainlayoutnew.addLayout(infoColumnnew, 1)
         mainlayoutnew.addLayout(messageColumnnew, 4)
-    
+        
         #mainlayout.addLayout(messageColumnnew, 0, 1, 0, 3) # Adds a  column for messages (sending and receiving) that spans 4 columns
-        #mainlayout.addLayout(infoColumnnew, 0, 0, 0, 0) # Adds a column for info that spans 1 column     
+        #mainlayout.addLayout(infoColumnnew, 0, 0, 0, 0) # Adds a column for info that spans 1 column 
+        
 
         # finally places the mainlayout
         centralWidget = QWidget() #empty widget to put grid into, code breaks if this isnt done
@@ -140,9 +170,12 @@ class MainWindow(QMainWindow):
         #centralWidget.setLayout(mainlayout)
         self.setCentralWidget(centralWidget)
 
-    def usersChanged(self):        
+
+    def usersChanged(self):
+        
         # get the user list from server OR have a variable that gets updated when user join/leave
         self.userList.setText("data from server")
+
 
     def connectToServer(self):
         print("test")
@@ -156,12 +189,10 @@ class MainWindow(QMainWindow):
                 self.messagesdisplay.append("Server Not Found")
                 return
             self.connectButton.setText("Disconnect")
-            #holy fuck this shit is wack, if this comment is still in my man is a vibe coder, but his vibes suck
             self.connected = True
             self.ipEntry.setEnabled(False)
             self.portEntry.setEnabled(False)
             self.usernameEntry.setEnabled(False)
-            self.coloursOptions.setEnabled(False)
             self.messageEntry.setEnabled(True)
             self.sendButton.setEnabled(True)
             
@@ -171,11 +202,14 @@ class MainWindow(QMainWindow):
             self.ipEntry.setEnabled(True)
             self.portEntry.setEnabled(True)
             self.usernameEntry.setEnabled(True)
-            self.coloursOptions.setEnabled(True)
             self.messageEntry.setEnabled(False)
             self.sendButton.setEnabled(False)
             self.abandonConnection()
     
+    
+    def resetUserList(self):
+        self.messagesdisplay.setPlainText("")
+
     def sendMessage(self):
         message = self.messageEntry.text()
 
@@ -185,78 +219,157 @@ class MainWindow(QMainWindow):
             self.messageEntry.setText("")
             if message == "/help":
                 self.messagesdisplay.append("Commands are:\n/help - Brings up this list\n/whisper or /w - privately message a user\n/clear - Clears the message log\n/profanity add/remove/toggle - Adds to or removes from the profanity or toggles it on or off\n/disconnect - If you can't manage to find the button yourself")
+            
+            elif message == "/whisper" or "/w":
+                self.sock.send(json.dumps({"type": "whisper", "to": "", "content": ""}))
+
             else:
                 self.messagesdisplay.append(f"'{message}' not found")
 
         else:
-            self.messageinfo = {
-                "type": "message",
-                "username": self.usernameEntry.text(),
-                "content": message,
-                "colour": "" # placeholder might add or remove
-            }
             print("sent")
             # This is where the message will be then sent as a dictionary
+            self.sock.send(json.dumps({"type": "message", "username": self.usernameEntry.text(), "content": message, "colour": ""}).encode())
             #self.messagesdisplay.append(f"<{self.messageinfo['username']}> {self.messageinfo['content']}") # change this to be parts of the json stuff
-            self.sock.send(json.dumps(self.messageinfo).encode())
             self.messageEntry.setText("")
+
+
 
     def closeEvent(self, event: QCloseEvent):
         print("dead")
         self.abandonConnection()
-        #if self.connected():
-        self.sock.close()
+
 
     def establishConnection(self):
         self.ip = str(self.ipEntry.text()) or "127.0.0.1"
         self.port = int(self.portEntry.text()) or 42069
-        self.callsign = self.usernameEntry.text() or "placeholder"
+        self.username = self.usernameEntry.text() or "placeholder"
         self.sock = s.socket(s.AF_INET, s.SOCK_STREAM) # creates an instance of socket, SOCK_STREAM makes it tcp/ip
         try:
             self.sock.connect((self.ip, self.port))
-            #self.sock.send(self.callsign.encode())
+            # self.sock.send(json.dumps({"type":"join", "username":self.username}).encode())  FOR WHEN IAN UPDATES THIS
+            self.sock.send(self.username.encode())
         except WindowsError as e:
+            print(e)
             return "error"
+        
         
         self.recvThread = threading.Thread(target=self.receiveThread, args=(self.sock,)) 
         self.recvThread.start()
 
         self.callsign = str(self.usernameEntry.text())
-        try:
-            self.sock.send(self.callsign.encode())
-        except Exception as e:
-            print(e)
-            return "error"
-    
 
-    def receiveThread(self, sock):        
-        while True:
+    def addUserRow(self, user):
+        position = self.userList.rowCount()
+        self.userList.insertRow(int(position))
+        self.userList.setItem(int(position), 0, QTableWidgetItem(user))
+    
+    def removeUserRow(self, user):
+        count = self.userList.rowCount()
+        for row in range(count):
+            item = self.userList.item(row, 0)
+            if item and item.text() == user:
+                self.userList.removeRow(row)
+                break
+
+    def receiveThread(self, sock):
+
+        while self.connected:
+            self.users = []
             try:
                 receivedMessagejson = sock.recv(4096).decode()
-                receivedMessage = json.loads(receivedMessagejson)
-                #receivedMessage = receivedMessage.strip()
-                if receivedMessage['type'] == 'message':
-                    self.messagesdisplay.append(f'<{receivedMessage['username']}> {receivedMessage['content']}') 
-                elif receivedMessage["type"] == "timeout":     
-                    self.messagesdisplay.append(f'{receivedMessage['msg']}') 
-                    print(receivedMessage)
-                elif receivedMessage["type"] == "welcome" or "join" or "leave":
-                    self.messagesdisplay.append(receivedMessage["content"])
-                    #self.userList.setPlainText("")
-                    for i in receivedMessage["users"]:
-                        self.userList.append(i)
+                self.receivedMessage = json.loads(receivedMessagejson)
+                print("Raw Input:", self.receivedMessage)
+                #self.receivedMessage = self.receivedMessage.strip()
                 
             except WindowsError:
-                self.messagesdisplay.append("Server has closed")
+                if self.connected:
+                    self.messagesdisplay.append("Server has closed")
+                
             except Exception as e:
-                print(f"Error: {e}")
+                print("Error")
+                print(e)
                 self.messagesdisplay.append(f"Error: {e}")
+                print("you stupid idiot mf 2")
+                time.sleep(1)
                 sock.close()
+            
+            try:
+                if not isinstance(self.receivedMessage, dict) or "type" not in self.receivedMessage:
+                    self.messagesdisplay.append("Error receiving message from server")
+                    print("Error Receiving:", self.receivedMessage)
+                    continue
+                elif self.receivedMessage.get("type") == "timeout":
+                    self.messagesdisplay.append(self.receivedMessage.get("content"))
+                    self.timeoutThread = threading.Thread(target=self.timeout)
+                    self.timeoutThread.start()
+
+                elif self.receivedMessage.get("type") == "welcome":
+                    self.messagesdisplay.append(self.receivedMessage.get("content"))
+
+                elif self.receivedMessage.get("type") == "join":
+                    userListString = []
+                    for i in self.receivedMessage.get("users"):
+                        print("User:", i)
+                        self.addUserRow(i)
+                
+                    
+                    
+                    self.messagesdisplay.append(self.receivedMessage.get("content"))
+                
+                elif self.receivedMessage.get("type") == "kick":
+                    self.messagesdisplay.append()
+                    self.messagesdisplay.append(self.receivedMessage.get("content"))
+                    
+
+                
+                elif self.receivedMessage.get("type") == "leave":
+                    self.removeUserRow(self.receivedMessage.get('users'))
+                    print(self.receivedMessage.get("users"))
+                    self.messagesdisplay.append(self.receivedMessage.get("content"))
+
+                
+                #elif self.receivedMessage.get("type") == "message":
+                #    self.messagesdisplay.append(f"<{self.receivedMessage.get('username')}> {self.receivedMessage.get('content')}")
+                
+
+                elif self.receivedMessage.get("type") == "message":
+                    self.messagesdisplay.append(f"<i>{self.receivedMessage.get('username')} whispers to you: {self.receivedMessage.get('content')}")
+            
+            except Exception as e:
+                print(e)
+                try:
+                    #self.messagesdisplay.append(self.receivedMessage)
+                    print()
+                except Exception as e:
+                    print(e)
+                    self.messagesdisplay.append("Error receiving message.")
     
+    def timeout(self, minutes):
+        self.messageEntry.setEnabled(False)
+        self.sendButton.setEnabled(False)
+        try:
+            time.sleep(int(minutes)*60) # minutes are given by the server but time.sleep works in seconds so it needs to be multiplied by 60
+        except:
+            self.messagesdisplay.append("huh there was an error timing you out, funny that")
+        self.messageEntry.setEnabled(True)
+        self.sendButton.setEnabled(True)
+
+
     def abandonConnection(self):
-        self.sock.close()
-        print("placeholder")
+        if self.sock:
+            try:
+                self.sock.shutdown(s.SHUT_RDWR)
+                self.sock.close()
+                self.messagesdisplay.append("<b>You have disconnected")
+            except Exception as e:
+                print(e)
+                pass
         
+        
+        
+
+
 if __name__ == "__main__": # checks if this file is being run
     app = QApplication(sys.argv)
     window = MainWindow()
@@ -266,3 +379,4 @@ if __name__ == "__main__": # checks if this file is being run
 
 
 
+ 
